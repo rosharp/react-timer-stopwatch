ee7230d62a5035accd367f7898a27b34a51cb45c
Ро <ro.sharp@MacBook-Pro-Ro.local>
2022-02-13 15:44:00 +1000

    Added .gitignore

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Stopwatch from "./Stopwatch";
+import Countdown from "./Countdown";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Timers Demo</div>
+        <div className="Timers">
+          <Stopwatch />
+          <Countdown />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
